37b37eb2cac5b5133a7fe468f8404d6de7de5608
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-03-27 18:21:05 -0300

    Add login form component

---
+++ src/app/(app)/entrar/components/Form/index.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import supabase from "@/services/supabase";
+import styles from '@/components/Wrappers/Auth/styles.module.css';
+
+function Form() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const router = useRouter();
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        setIsLoading(true);
+        setError(null);
+
+        const {error} = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        setIsLoading(false);
+
+        if (error?.status === 400 && error?.code === 'invalid_credentials') {
+            setError('E-mail e/ou senha incorretos');
+            return;
+        }
+
+        if (error !== null) {
+            setError('Ocorreu um erro, tente novamente mais tarde');
+            return;
+        }
+
+        router.push('/painel');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {error !== null && (
+                <p className={styles.error}>
+                    {error}
+                </p>
+            )}
+
+            <input
+                onChange={({target}) => setEmail(target.value)}
+                placeholder="E-mail"
+                required
+                type="email"
+                value={email}
+            />
+
+            <input
+                onChange={({target}) => setPassword(target.value)}
+                placeholder="Senha"
+                required
+                type="password"
+                value={password}
+            />
+
+            <button
+                disabled={(
+                    email.trim().length === 0 ||
+                    password.trim().length === 0 ||
+                    isLoading === true
+                )}
+                type="submit"
+            >
+                {isLoading ? 'ENTRANDO...' : 'ENTRAR'}
+            </button>
+        </form>
+    );
+}
+
+export default Form;
